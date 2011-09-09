40c89cb15eb9088723c823a9669eee6e741c9288
Serges Moreau <mrserges@gmail.com>
2011-09-08 20:06:21 -0400

    blog file

--- lib/blog.rb
+++ lib/blog.rb
@@ -1,3 +1,14 @@
 #
 # Create your "Blog" class in this file.
 #
+
+class Blog
+  def initialize input_url = nil
+    @url = input_url
+  end
+
+  def latest_title
+    Hpricot(Net::HTTP.get(URI.parse(@url)))
+    .search("/html/body/div[2]/div/div[2]/div/div/h3/").first.to_s
+  end
+end
